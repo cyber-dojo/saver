87e3a91c3b38e5bb4a3ffd7ba0529346b9aa3e1f
Jon Jagger <jon@jaggersoft.com>
2018-10-06 10:45:12 +0100

    refactoring; drop unneeded @@

--- server/src/base58.rb
+++ server/src/base58.rb
@@ -37,10 +37,10 @@ class Base58
   end
 
   def self.alphabet
-    @@ALPHABET
+    ALPHABET
   end
 
-  @@ALPHABET = %w{
+  ALPHABET = %w{
     0 1 2 3 4 5 6 7 8 9
     A B C D E F G H   J K L M N   P Q R S T U V W X Y Z
     a b c d e f g h   j k l m n   p q r s t u v w x y z
