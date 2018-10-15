01bf87ecad9f17bf3bbc775d0108944efc3880cb
Jon Jagger <jon@jaggersoft.com>
2018-10-15 10:44:36 +0100

    delete dead code

--- server/test/coverage.rb
+++ server/test/coverage.rb
@@ -4,7 +4,6 @@ cov_root = File.expand_path('..', File.dirname(__FILE__))
 
 SimpleCov.start do
   #add_group('debug') { |src| print src.filename+"\n"; false }
-  add_filter 'src/micro_service.rb' # done from client container
   add_group('src')      { |src| src.filename.start_with? "#{cov_root}/src" }
   add_group('test/src') { |src| src.filename.start_with? "#{cov_root}/test/src" }
 end
