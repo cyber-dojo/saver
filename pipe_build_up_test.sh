838078d7c9ad320daa405510bc938e5c62e54ec6
Jon Jagger <jon@jaggersoft.com>
2018-10-22 08:11:03 +0100

    move storer test data under test/ dir

--- pipe_build_up_test.sh
+++ pipe_build_up_test.sh
@@ -7,7 +7,8 @@ readonly SH_DIR="${ROOT_DIR}/sh"
 
 "${SH_DIR}/build_docker_images.sh"
 "${SH_DIR}/docker_containers_up.sh"
-"${ROOT_DIR}/katas_old/tar_pipe_in.sh"
+
+"${ROOT_DIR}/server/test/storer_katas/old/tar_pipe_in.sh"
 
 if "${SH_DIR}/run_tests_in_containers.sh" "$@"; then
   "${SH_DIR}/docker_containers_down.sh"
