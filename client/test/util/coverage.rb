1e75d6e730cab3f52255167292522c1ada5627c3
Jon Jagger <jon@jaggersoft.com>
2018-12-17 12:09:48 +0000

    refactoring

--- client/test/util/coverage.rb
+++ client/test/util/coverage.rb
@@ -4,13 +4,13 @@ cov_root = File.expand_path('../..', File.dirname(__FILE__))
 
 SimpleCov.start do
   #add_group('debug') { |code| print code.filename+"\n"; false }
-  add_group('src') { |code|
-    code.filename.start_with? "#{cov_root}/src"
+  add_group('src') { |src|
+    src.filename.start_with?("#{cov_root}/src")
   }
-  add_group('test') { |code|
-    code.filename.start_with? "#{cov_root}/test"
+  add_group('test') { |test|
+    test.filename.start_with?("#{cov_root}/test")
   }
 end
 
-SimpleCov.root cov_root
-SimpleCov.coverage_dir ENV['COVERAGE_ROOT']
+SimpleCov.root(cov_root)
+SimpleCov.coverage_dir(ENV['COVERAGE_ROOT'])
