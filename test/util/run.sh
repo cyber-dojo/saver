12eef83c97e5b82471bca57d9e3aa66b978092fc
Jon Jagger <jon@jaggersoft.com>
2018-12-17 11:59:57 +0000

    refactoring

--- test/util/run.sh
+++ test/util/run.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
-readonly FILES=(${MY_DIR}/../*_test.rb)
+readonly TEST_FILES=(${MY_DIR}/../*_test.rb)
 readonly TEST_LOG=${COVERAGE_ROOT}/test.log
 readonly ARGS=(${*})
 
 mkdir -p ${COVERAGE_ROOT}
 
-ruby -e "([ '${MY_DIR}/coverage.rb' ] + %w(${FILES[*]})).each{ |file| require file }" \
-  -- ${ARGS[@]} | tee ${TEST_LOG}
+ruby \
+  -e "([ '${MY_DIR}/coverage.rb' ] + \
+         %w(${TEST_FILES[*]})        \
+      ).each{ |file| require file }" \
+  -- ${ARGS[@]}                      \
+  | tee ${TEST_LOG}
 
 ruby ${MY_DIR}/check_test_results.rb \
   ${TEST_LOG} \
