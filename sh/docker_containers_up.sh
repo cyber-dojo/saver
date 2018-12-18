25dc8e28741a68f312ce0bb4b8eca8a734d66a92
Jon Jagger <jon@jaggersoft.com>
2018-12-18 05:57:48 +0000

    refactoring; delete unneeded services

--- sh/docker_containers_up.sh
+++ sh/docker_containers_up.sh
@@ -33,5 +33,3 @@ docker-compose \
 wait_till_up "test-${MY_NAME}-server"
 wait_till_up "test-${MY_NAME}-client"
 wait_till_up "test-${MY_NAME}-starter"
-wait_till_up "test-${MY_NAME}-prometheus"
-wait_till_up "test-${MY_NAME}-grafana"
