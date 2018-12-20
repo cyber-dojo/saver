ccfa461f9768d6249a9f790f9fa7934b737097db
Jon Jagger <jon@jaggersoft.com>
2018-12-20 14:22:38 +0000

    make saver user a member of the group nogroup

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,11 @@ FROM  cyberdojo/rack-base
 LABEL maintainer=jon@jaggersoft.com
 
 RUN adduser \
-  -D       `# no password` \
-  -H       `# no home dir` \
-  -u 19663 `# user-id`     \
-  saver    `# user-name`
+  -D         `# no password` \
+  -H         `# no home dir` \
+  -u 19663   `# user-id`     \
+  -G nogroup `# group`       \
+  saver      `# user-name`
 
 COPY . /app
 RUN chown -R saver /app
---
+++ test/identity_test.rb
@@ -0,0 +1,35 @@
+require_relative 'test_base'
+
+class IdentityTest < TestBase
+
+  def self.hex_prefix
+    '1B5'
+  end
+
+  # - - - - - - - - - - - - - - - - -
+
+  test '2C6',
+  'uid=19663(saver), gid=65533(nogroup)' do
+    assert_equal %w( saver 19663 ), [user_name,uid]
+    assert_equal %w( nogroup 65533 ), [group_name,gid]
+  end
+
+  private
+
+  def user_name
+    `whoami`.strip
+  end
+
+  def group_name
+    `getent group #{gid}`.split(':')[0]
+  end
+
+  def uid
+    `id -u #{user_name}`.strip
+  end
+
+  def gid
+    `id -g #{user_name}`.strip
+  end
+
+end
