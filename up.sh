5ef7329b8f8672028262dfb450bb3fa2ddb9df76
Jon Jagger <jon@jaggersoft.com>
2018-12-24 17:11:56 +0000

    ensure docker logs are clean on startup

--- up.sh
+++ up.sh
@@ -49,7 +49,11 @@ if [[ ! -d /${dir}/katas ]]; then
   mkdir /${dir}/katas
 fi
 
-bundle exec rackup \
+# HOME=/tmp is needed to avoid warnings such as
+#   Bundler will use `/tmp/bundler/home/unknown' as your home directory temporarily
+# This is important for tests that assert on container log contents at startup.
+
+HOME=/tmp bundle exec rackup \
   --warn           \
   --host 0.0.0.0   \
   --port 4537      \
