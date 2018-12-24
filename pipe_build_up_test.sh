08d48fcd64d33b860f745c92677e962995a1c750
Jon Jagger <jon@jaggersoft.com>
2018-12-24 17:29:56 +0000

    refactoring; simplify script

--- pipe_build_up_test.sh
+++ pipe_build_up_test.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -e
 
-readonly ROOT_DIR="$( cd "$( dirname "${0}" )" && pwd )"
-readonly SH_DIR="${ROOT_DIR}/sh"
+readonly SH_DIR="$( cd "$( dirname "${0}" )" && pwd )/sh"
 
 "${SH_DIR}/build_docker_images.sh"
 "${SH_DIR}/docker_containers_up.sh"
