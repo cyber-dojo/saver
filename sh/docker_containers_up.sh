b0eabcf98a0279bd1f119f56d441448d933dd5ba
Jon Jagger <jon@jaggersoft.com>
2018-12-25 05:41:03 +0000

    refactoring; drop unneeded [bundle exec] in up.sh; do proper service ready check; check for clean service startup

--- sh/docker_containers_up.sh
+++ sh/docker_containers_up.sh
@@ -1,28 +1,73 @@
 #!/bin/bash
 set -e
 
-readonly ROOT_DIR="$( cd "$( dirname "${0}" )" && cd .. && pwd )"
-readonly MY_NAME=saver
-
 # - - - - - - - - - - - - - - - - - - - -
+
+wait_till_ready()
+{
+  local name="${1}"
+  local port="${2}"
+  local max_tries=20
+  local cmd="curl --silent --fail --data '{}' -X GET http://localhost:${port}/sha"
+  cmd+=" > /dev/null 2>&1"
+
+  if [ ! -z ${DOCKER_MACHINE_NAME} ]; then
+    cmd="docker-machine ssh ${DOCKER_MACHINE_NAME} ${cmd}"
+  fi
+  echo -n "Checking ${name} is ready"
+  while [ $(( max_tries -= 1 )) -ge 0 ] ; do
+    echo -n '.'
+    if eval ${cmd} ; then
+      echo 'OK'
+      return
+    else
+      sleep 0.1
+    fi
+  done
+  echo 'FAIL'
+  echo "${name} not ready after 20 tries"
+  docker logs ${name}
+  exit 1
+}
+
+# - - - - - - - - - - - - - - - - - - -
 
 wait_till_up()
 {
-  local n=10
+  local name="${1}"
+  local n=20
   while [ $(( n -= 1 )) -ge 0 ]
   do
-    if docker ps --filter status=running --format '{{.Names}}' | grep -q ^${1}$ ; then
+    if docker ps --filter status=running --format '{{.Names}}' | grep -q ^${name}$ ; then
       return
     else
-      sleep 0.5
+      sleep 0.1
     fi
   done
-  echo "${1} not up after 5 seconds"
+  echo "${name} not up after 20 tries"
   docker logs "${1}"
   exit 1
 }
 
 # - - - - - - - - - - - - - - - - - - - -
+
+exit_unless_started_cleanly()
+{
+  local name="${1}"
+  local docker_logs=$(docker logs "${name}")
+  if [[ ! -z "${docker_logs}" ]]; then
+    echo "[docker logs ${name}] not empty on startup"
+    echo "<docker_logs>"
+    echo "${docker_logs}"
+    echo "</docker_logs>"
+    exit 1
+  fi
+}
+
+# - - - - - - - - - - - - - - - - - - - -
+
+readonly ROOT_DIR="$( cd "$( dirname "${0}" )" && cd .. && pwd )"
+readonly MY_NAME=saver
 
 docker-compose \
   --file "${ROOT_DIR}/docker-compose.yml" \
@@ -30,6 +75,8 @@ docker-compose \
   -d \
   --force-recreate
 
-wait_till_up "test-${MY_NAME}-server"
+wait_till_ready "test-${MY_NAME}-server" 4537
+exit_unless_started_cleanly "test-${MY_NAME}-server"
+
 wait_till_up "test-${MY_NAME}-client"
 wait_till_up "test-${MY_NAME}-starter"
--- up.sh
+++ up.sh
@@ -49,11 +49,7 @@ if [[ ! -d /${dir}/katas ]]; then
   mkdir /${dir}/katas
 fi
 
-# HOME=/tmp is needed to avoid warnings such as
-#   Bundler will use `/tmp/bundler/home/unknown' as your home directory temporarily
-# This is important for tests that assert on container log contents at startup.
-
-HOME=/tmp bundle exec rackup \
+rackup             \
   --warn           \
   --host 0.0.0.0   \
   --port 4537      \
