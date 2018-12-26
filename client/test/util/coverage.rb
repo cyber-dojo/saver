d0fc0250cc662c9199bfa5a7ccec737f7b971d8a
Jon Jagger <jon@jaggersoft.com>
2018-12-26 07:16:23 +0000

    refactoring; delete dead statement

--- client/test/util/coverage.rb
+++ client/test/util/coverage.rb
@@ -1,16 +1,9 @@
 require 'simplecov'
 
 cov_root = File.expand_path('../..', File.dirname(__FILE__))
-
 SimpleCov.start do
-  #add_group('debug') { |code| print(code.filename+"\n"); false }
-  add_group('src') { |src|
-    src.filename.start_with?("#{cov_root}/src")
-  }
-  add_group('test') { |test|
-    test.filename.start_with?("#{cov_root}/test")
-  }
+  add_group( 'src') { | src|  src.filename.start_with?("#{cov_root}/src")  }
+  add_group('test') { |test| test.filename.start_with?("#{cov_root}/test") }
 end
-
 SimpleCov.root(cov_root)
 SimpleCov.coverage_dir(ENV['COVERAGE_ROOT'])
