296d9418ce691ab1a3f570f6eee98047b72089a2
Jon Jagger <jon@jaggersoft.com>
2019-01-04 15:27:48 +0000

    put dir code into disk file

--- src/external_disk.rb
+++ src/external_disk.rb
@@ -1,9 +1,61 @@
-require_relative 'external_dir'
+require 'open3'
 
 class ExternalDisk
 
   def [](name)
     ExternalDir.new(name)
+  end
+
+end
+
+# - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+class ExternalDir
+
+  def initialize(name)
+    @name = name
+  end
+
+  attr_reader :name
+
+  def exists?
+    File.directory?(name)
+  end
+
+  def make
+    # Returns true iff the dir does not already exist
+    # and is made. Can't find a Ruby library method
+    # that does this, so using shell.
+    # Note: FileUtils.mkdir_p() does not tell.
+    # -p creates intermediate dirs as required.
+    # -v verbose mode, output each dir actually made
+    stdout,stderr,r = Open3.capture3("mkdir -vp '#{name}'")
+    stdout != '' && stderr == '' && r.exitstatus == 0
+  end
+
+  # - - - - - - - - - - - - - - - - - - - - - - - -
+
+  def append(filename, content)
+    open(filename, 'a') { |fd| fd.write(content) }
+  end
+
+  def write(filename, content)
+    open(filename, 'w') { |fd| fd.write(content) }
+  end
+
+  def read(filename)
+    open(filename, 'r') { |fd| fd.read }
+  end
+
+  private
+
+  def open(filename, mode)
+    File.open(pathed(filename), mode) { |fd| yield fd }
+  end
+
+  def pathed(filename)
+    File.join(name, filename)
   end
 
 end
