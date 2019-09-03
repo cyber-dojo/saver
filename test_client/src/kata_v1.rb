5dcec95f5c0384c1eb0251b5b443a40c1b68ad8f
JonJagger <jon@jaggersoft.com>
2019-09-03 09:52:44 +0100

    Update client-side kata_v1/liner

--- test_client/src/kata_v1.rb
+++ test_client/src/kata_v1.rb
@@ -27,9 +27,9 @@ class Kata_v1
     }
     saver.batch([
       create_cmd(id, 0),
-      manifest_write_cmd(id, manifest),
-      event_write_cmd(id, 0, { 'files' => files }),
-      events_write_cmd(id, event0)
+      manifest_write_cmd(id, json_plain(manifest)),
+      event_write_cmd(id, 0, json_plain(lined({ 'files' => files }))),
+      events_write_cmd(id, json_plain(event0) + "\n")
     ])
     # TODO: unless result === [true]*4
     id
@@ -46,7 +46,7 @@ class Kata_v1
       fail invalid('id', id)
     end
     manifest = json_parse(manifest_src)
-    event0 = unlined(event0_src)
+    event0 = unlined(json_parse(event0_src))
     manifest['visible_files'] = event0['files']
     manifest
   end
@@ -71,13 +71,13 @@ class Kata_v1
     result = saver.batch([
       exists_cmd(id),
       create_cmd(id, index),
-      event_write_cmd(id, index, event_n),
-      events_append_cmd(id, event_summary)
+      event_write_cmd(id, index, json_plain(lined(event_n))),
+      events_append_cmd(id, json_plain(event_summary) + "\n")
     ])
     unless result[0]
       fail invalid('id', id)
     end
-    unless result[1]
+    unless result === [true]*4
       fail invalid('index', index)
     end
     nil
@@ -109,7 +109,7 @@ class Kata_v1
     if event_src.nil?
       fail invalid('index', index)
     end
-    unlined(event_src)
+    unlined(json_parse(event_src))
   end
 
   private
@@ -155,8 +155,8 @@ class Kata_v1
   # start-point services. In practice, it doesn't work because the
   # start-point services can change over time.
 
-  def manifest_write_cmd(id, manifest)
-    ['write', manifest_filename(id), json_plain(manifest)]
+  def manifest_write_cmd(id, manifest_src)
+    ['write', manifest_filename(id), manifest_src]
   end
 
   def manifest_read_cmd(id)
@@ -176,12 +176,12 @@ class Kata_v1
   # This is an optimization for ran_tests() which need only
   # append to the end of the file.
 
-  def events_write_cmd(id, event0)
-    ['write', events_filename(id), json_plain(event0) + "\n"]
+  def events_write_cmd(id, event0_src)
+    ['write', events_filename(id), event0_src]
   end
 
-  def events_append_cmd(id, event)
-    ['append', events_filename(id), json_plain(event) + "\n"]
+  def events_append_cmd(id, eventN_src)
+    ['append', events_filename(id), eventN_src]
   end
 
   def events_read_cmd(id)
@@ -198,8 +198,8 @@ class Kata_v1
   # The visible-files are stored in a lined-format so they be easily
   # inspected on disk. Have to be unlined when read back.
 
-  def event_write_cmd(id, index, event)
-    ['write', event_filename(id, index), json_plain(lined(event))]
+  def event_write_cmd(id, index, event_src)
+    ['write', event_filename(id, index), event_src]
   end
 
   def event_read_cmd(id, index)
@@ -208,21 +208,6 @@ class Kata_v1
 
   def event_filename(id, index)
     id_path(id, index, 'event.json')
-  end
-
-  def lined(event)
-    event['files'] = lined_files(event['files'])
-    lined_file(event['stdout'])
-    lined_file(event['stderr'])
-    event
-  end
-
-  def unlined(event_src)
-    event = json_parse(event_src)
-    event['files'] = unlined_files(event['files'])
-    unlined_file(event['stdout'])
-    unlined_file(event['stderr'])
-    event
   end
 
   # - - - - - - - - - - - - - -
--- test_client/src/liner.rb
+++ test_client/src/liner.rb
@@ -2,31 +2,69 @@
 
 module Liner # mix-in
 
-  def lined_files(files)
-    files.each {|_filename,file| lined_file(file) }
+  def lined(event)
+    result = {}
+    result['files'] = lined_files(event['files'])
+    if event.has_key?('stdout')
+      result['stdout'] = lined_file(event['stdout'])
+    end
+    if event.has_key?('stderr')
+      result['stderr'] = lined_file(event['stderr'])
+    end
+    if event.has_key?('status')
+      result['status'] = event['status']
+    end
+    result
   end
 
-  def unlined_files(files)
-    files.each {|_filename,file| unlined_file(file) }
+  def lined_files(files)
+    Hash[files.map{ |filename,file|
+      [filename,lined_file(file)]
+    }]
   end
 
   def lined_file(file)
-    unless file.nil?
-      content = file['content']
-      if content == ''
-        # [ '' ] is more visually obvious than []
-        # Note: [''].join == [].join == ''
-        file['content'] = [ '' ]
-      else
-        file['content'] = content.lines
-      end
+    content = file['content']
+    lined = {
+      'content' => (content=='') ? [ '' ] : content.lines
+    }
+    if file.has_key?('truncated')
+      lined['truncated'] = file['truncated']
     end
+    lined
+  end
+
+  # - - - - - - - - - - - - - - - - - -
+
+  def unlined(event)
+    result = {}
+    result['files'] = unlined_files(event['files'])
+    if event.has_key?('stdout')
+      result['stdout'] = unlined_file(event['stdout'])
+    end
+    if event.has_key?('stderr')
+      result['stderr'] = unlined_file(event['stderr'])
+    end
+    if event.has_key?('status')
+      result['status'] = event['status']
+    end
+    result
+  end
+
+  def unlined_files(files)
+    Hash[files.map{ |filename,file|
+      [filename,unlined_file(file)]
+    }]
   end
 
   def unlined_file(file)
-    unless file.nil?
-      file['content'] = file['content'].join
+    unlined = {
+      'content' => file['content'].join
+    }
+    if file.has_key?('truncated')
+      unlined['truncated'] = file['truncated']
     end
+    unlined
   end
 
 end
