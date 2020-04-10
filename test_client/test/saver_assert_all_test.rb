117eef4e4fbfe01b3e29475a5ed7b7a860b2d149
Jon Jagger <jon@jaggersoft.com>
2020-04-10 08:04:10 +0100

    Add client-side test for assert_all() completing all its commands

---
+++ test_client/test/saver_assert_all_test.rb
@@ -0,0 +1,66 @@
+require_relative 'test_base'
+require_relative '../src/saver_service'
+require_relative 'saver_service_fake'
+
+class SaverAssertAllTest < TestBase
+
+  def self.hex_prefix
+    'AA2'
+  end
+
+  def hex_setup
+    @expected = []
+    @commands = []
+  end
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  multi_test '416', %w(
+  |assert_all() returns array of results
+  |when all commands are true
+  ) do
+    dirname = 'client/assert-all/e3/t4/16'
+    command(true, dir_make_command(dirname))
+    command(true, dir_exists_command(dirname))
+    there_yes = dirname + '/there-yes.txt'
+    content = 'dunkeld tay beat'
+    command(true, file_create_command(there_yes, content))
+    command(true, file_append_command(there_yes, content.reverse))
+    command(content+content.reverse, file_read_command(there_yes))
+    result = saver.assert_all(@commands)
+    assert_equal @expected, result
+  end
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - -
+
+=begin
+  multi_test '417', %w(
+  |assert_all() raises
+  |when any command is not true
+  |and subsequent commands are not executed
+  ) do
+    dirname = 'client/assert-all/e3/t4/17'
+    command(true, dir_make_command(dirname))
+    command(true, dir_exists_command(dirname))
+    there_yes = dirname + '/there-yes.txt'
+    content = 'monaltrie dee beat'
+    command(true, file_create_command(there_yes, content))
+    there_no = dirname + '/there-not.txt'
+    command(false, file_read_command(there_no))
+    command(true, file_append_command(there_yes, content.reverse))
+    error = assert_raises(SaverService::Error) {
+      saver.assert_all(@commands)
+    }
+    assert_equal "commands[3] != true", error.message
+    assert_equal content, saver.run(saver.file_read_command(there_yes)), :does_not_execute_subsequent_commands
+  end
+=end
+
+  private
+
+  def command(expected, cmd)
+    @expected << expected
+    @commands << cmd
+  end
+
+end
