b36548ea4df3e977cfc04c63ea983ce86513e7fb
Jon Jagger <jon@jaggersoft.com>
2020-04-10 17:42:42 +0100

    Split client-side run_until_true/run_until_false tests into separate files

--- test_client/test/saver_run_until_false_test.rb
+++ test_client/test/saver_run_until_false_test.rb
@@ -3,7 +3,7 @@ require_relative 'test_base'
 require_relative '../src/saver_service'
 require_relative 'saver_service_fake'
 
-class SaverRunManyTest < TestBase
+class SaverRunUntilFalseTest < TestBase
 
   def self.hex_prefix
     '86A'
@@ -130,137 +130,6 @@ class SaverRunManyTest < TestBase
     assert_equal content, file_read(filename), :does_not_execute_subsequent_commands
   end
 
-  # - - - - - - - - - - - - - - - - - - - - - - - - -
-  # run_until_true
-  # - - - - - - - - - - - - - - - - - - - - - - - - -
-
-  multi_test '712', %w(
-  |run_until_true()
-  |completes all its commands
-  }when nothing returns true
-  ) do
-    dirname = 'client/batch-run-until-true/x3/t7/12'
-    filename = dirname + '/read-false.txt'
-    command(false, file_read_command(filename+'1'))
-    command(false, file_read_command(filename+'2'))
-    command(false, file_read_command(filename+'3'))
-    command(false, file_read_command(filename+'4'))
-    command(false, file_read_command(filename+'5'))
-    assert_run_until_true
-  end
-
-  # - - - - - - - - - - - - - - - - - - - - - - - - -
-
-  multi_test '713', %w(
-  |run_until_true()
-  |stops at exists?() returning true
-  |and does not execute subsequent commands
-  ) do
-    dirname = 'client/batch-run-until-true/x3/t7/13'
-    assert saver.create(dirname)
-    command(false, dir_exists_command(dirname+'1'))
-    command(false, dir_exists_command(dirname+'2'))
-    command(false, dir_exists_command(dirname+'3'))
-    command(true,  dir_exists_command(dirname))
-    filename = dirname + '/stops-at-exists-true'
-    not_run(file_create_command(filename, 'xxx'))
-    assert_run_until_true
-    refute file_read(filename), :does_not_execute_subsequent_commands
-  end
-
-  # - - - - - - - - - - - - - - - - - - - - - - - - -
-
-  multi_test '714', %w(
-  |run_until_true()
-  |stops at create() returning true
-  |and does not execute subsequent commands
-  ) do
-    dirname = 'client/batch-run-until-true/x3/t7/14'
-    assert saver.create(dirname)
-    command(false, dir_make_command(dirname))
-    command(false, dir_make_command(dirname))
-    command(false, dir_make_command(dirname))
-    command(true,  dir_make_command(dirname+'1'))
-    filename = dirname + '1/stops-at-create-true'
-    not_run(file_create_command(filename, 'xxx'))
-    assert_run_until_true
-    assert dir_exists?(dirname+'1'), :does_not_execute_subsequent_commands
-    refute file_read(filename), :does_not_execute_subsequent_commands
-  end
-
-  # - - - - - - - - - - - - - - - - - - - - - - - - -
-
-  multi_test '715', %w(
-  |run_until_true()
-  |stops at write() returning true
-  |and does not execute subsequent commands
-  ) do
-    dirname = 'client/batch-run-until-true/x3/t7/15'
-    filename = dirname + '/stop-at-write-true'
-    content = 'xxxxxx'
-    saver.assert_all([
-      saver.dir_make_command(dirname),
-      saver.file_create_command(filename, content)
-    ])
-
-    command(false, file_create_command(filename, content))
-    command(true, file_create_command(filename+'1', content))
-    not_run(file_append_command(filename, 'to-the-end'))
-    assert_run_until_true
-    assert_equal content, file_read(filename), :does_not_execute_subsequent_commands
-  end
-
-  # - - - - - - - - - - - - - - - - - - - - - - - - -
-
-  multi_test '716', %w(
-  |run_until_true()
-  |stops at append() returning true
-  |and does not execute subsequent commands
-  ) do
-    dirname = 'client/batch-run-until-true/x3/t7/16'
-    filename = dirname + '/stop-at-append-true'
-    content = 'X'
-
-    saver.assert_all([
-      saver.dir_make_command(dirname),
-      saver.file_create_command(filename, content)
-    ])
-
-    command(false, file_append_command(filename+'1', content))
-    command(true, file_append_command(filename, content))
-    not_run(file_append_command(filename, 'to-the-end'))
-    assert_run_until_true
-    assert_equal 'XX', file_read(filename), :does_not_execute_subsequent_commands
-  end
-
-  # - - - - - - - - - - - - - - - - - - - - - - - - -
-
-  multi_test '717', %w(
-  |run_until_true()
-  |stops at read() a file that exists
-  |and does not execute subsequent commands
-  ) do
-    dirname = 'client/batch-run-until-true/x3/t7/17'
-    event_3 = dirname + '/3.event.json'
-    content = '{"colour":"red"}'
-
-    saver.assert_all([
-      saver.dir_make_command(dirname),
-      saver.file_create_command(event_3,content)
-    ])
-
-    command(false, file_create_command(dirname + '/3.event.json', '{"colour":"amber"}'))
-    command(false, file_read_command(dirname + '/0.event.json'))
-    command(false, file_read_command(dirname + '/1.event.json'))
-    command(false, file_read_command(dirname + '/2.event.json'))
-    command(content, file_read_command(dirname + '/3.event.json'))
-    not_run(file_create_command(dirname + '/4.event.json', '{"colour":"green"}'))
-
-    assert_run_until_true
-    refute file_read(dirname + '/4.event.json'), :does_not_execute_subsequent_commands
-  end
-
-
   private
 
   def command(expected, cmd)
@@ -274,11 +143,6 @@ class SaverRunManyTest < TestBase
 
   def assert_run_until_false
     result = saver.run_until_false(@commands)
-    assert_equal @expected, result
-  end
-
-  def assert_run_until_true
-    result = saver.run_until_true(@commands)
     assert_equal @expected, result
   end
 
