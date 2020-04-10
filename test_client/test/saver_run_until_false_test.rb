3d3acbe26a2ad7929f4825a7d6ec655789b86683
Jon Jagger <jon@jaggersoft.com>
2020-04-10 17:46:26 +0100

    Add client-side tests for expected exceptions when run_until_true/false() is called with malformed commands args

--- test_client/test/saver_run_until_false_test.rb
+++ test_client/test/saver_run_until_false_test.rb
@@ -130,6 +130,72 @@ class SaverRunUntilFalseTest < TestBase
     assert_equal content, file_read(filename), :does_not_execute_subsequent_commands
   end
 
+  # - - - - - - - - - - - - - - - - - - - - - - - - -
+  # raises when malformed commands
+
+  multi_test '121', %w(
+  |when commands is not an Array
+  |raise
+  ) do
+    commands = 23
+    message = 'malformed:commands:!Array (Integer):'
+    assert_raises_SaverException(message,commands) {
+      saver.run_until_false(commands)
+    }
+  end
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  multi_test '122', %w(
+  |when commands entry is not an Array
+  |raise
+  ) do
+    commands = [42]
+    message = 'malformed:commands[0]:!Array (Integer):'
+    assert_raises_SaverException(message,commands) {
+      saver.run_until_false(commands)
+    }
+  end
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  multi_test '123', %w(
+  |when commands entry is unknown
+  |raise
+  ) do
+    commands = [['hgttg']]
+    message = 'malformed:commands[0]:Unknown (hgttg):'
+    assert_raises_SaverException(message,commands) {
+      saver.run_until_false(commands)
+    }
+  end
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  multi_test '124', %w(
+  |when commands entry is incorrect arity
+  |raises
+  ) do
+    commands = [['dir_make','abc/d/e/f'],['file_read',1,2,3,4,5,6]]
+    message = 'malformed:commands[1]:file_read!6:'
+    assert_raises_SaverException(message,commands) {
+      saver.run_until_false(commands)
+    }
+  end
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  multi_test '125', %w(
+  |when commands entry has non String parameter
+  |raises
+  ) do
+    commands = [['dir_exists?','abc/d/e/f'],['dir_make',nil]]
+    message = 'malformed:commands[1]:dir_make(dirname!=String):'
+    assert_raises_SaverException(message,commands) {
+      saver.run_until_false(commands)
+    }
+  end
+
   private
 
   def command(expected, cmd)
@@ -154,6 +220,18 @@ class SaverRunUntilFalseTest < TestBase
 
   def file_read(filename)
     saver.run(file_read_command(filename))
+  end
+
+  # - - - - - - - - - - - - - - - -
+
+  def assert_raises_SaverException(message,commands)
+    error = assert_raises(SaverService::Error) { yield }
+    json = JSON.parse!(error.message)
+    assert_equal '/run_until_false', json['path'], :path
+    expected_body = { 'commands'=>commands }
+    assert_equal expected_body, JSON.parse!(json['body']), :body
+    assert_equal 'SaverService', json['class'], :class
+    assert_equal message, json['message'], :message
   end
 
 end
