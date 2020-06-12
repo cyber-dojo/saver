188ee336c4de266d83327baec9d524c2acb51c44
Jon Jagger <jon@jaggersoft.com>
2020-06-12 15:21:13 +0100

    In k8s deployment, use helm_upgrade() function from new k8s-install repo.

--- .circleci/deploy_to_namespace.sh
+++ .circleci/deploy_to_namespace.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -Eeu
 
-source ./.circleci/helm_upgrade.sh
+source <(curl https://raw.githubusercontent.com/cyber-dojo/k8s-install/master/sh/helm_upgrade_probe_yes_prometheus_no.sh)
 
 # misc env-vars are in ci context
 
@@ -30,8 +30,8 @@ export $(curl https://raw.githubusercontent.com/cyber-dojo/versioner/master/app/
 readonly NAMESPACE="${1}" # beta|prod
 readonly CYBER_DOJO_SAVER_TAG="${CIRCLE_SHA1:0:7}"
 
-helm_upgrade \
-   "${NAMESPACE}" "saver" "praqma/cyber-dojo-service --version 0.2.5" \
+helm_upgrade_probe_yes_prometheus_no \
+   "${NAMESPACE}" "saver" \
    "${CYBER_DOJO_SAVER_IMAGE}" \
    "${CYBER_DOJO_SAVER_TAG}" \
    "${CYBER_DOJO_SAVER_PORT}" \
