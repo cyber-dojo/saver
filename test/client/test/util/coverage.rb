dac0ea0689df7ca9f1f7f91198cdc041873fb463
Jon Jagger <jon@jaggersoft.com>
2021-04-18 07:32:19 +0100

    Rename coverage tab name from src to code (client)

--- test/client/test/util/coverage.rb
+++ test/client/test/util/coverage.rb
@@ -2,7 +2,7 @@ require 'simplecov'
 
 cov_root = File.expand_path('../..', __dir__)
 SimpleCov.start do
-  add_group( 'src') { | src|  src.filename.start_with?("#{cov_root}/source" ) }
+  add_group('code') { |code| code.filename.start_with?("#{cov_root}/source" ) }
   add_group('test') { |test| test.filename.start_with?("#{cov_root}/test") }
 end
 SimpleCov.root(cov_root)
--- test/server/util/coverage.rb
+++ test/server/util/coverage.rb
@@ -1,7 +1,6 @@
 require 'simplecov'
 
 cov_root = File.expand_path('../..', __dir__)
-
 SimpleCov.start do
   add_group('code') { |code| code.filename.start_with?("#{cov_root}/source" ) }
   add_group('test') { |test| test.filename.start_with?("#{cov_root}/test") }
