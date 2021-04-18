bcf4ad3e40affbf996fca84fffe00fcfa3207794
Jon Jagger <jon@jaggersoft.com>
2021-04-18 06:57:48 +0100

    Rename sh/ to scripts/

--- build_test_publish.sh
+++ build_test_publish.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -Eeu
 
-readonly SH_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/sh" && pwd )"
+readonly SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/scripts" && pwd )"
 
-source ${SH_DIR}/versioner_env_vars.sh
+source "${SCRIPTS_DIR}/versioner_env_vars.sh"
 export $(versioner_env_vars)
-"${SH_DIR}/build_docker_images.sh"
-"${SH_DIR}/tag_image.sh"
-"${SH_DIR}/docker_containers_up.sh"
-"${SH_DIR}/run_tests_in_containers.sh" "$@"
-"${SH_DIR}/docker_containers_down.sh"
-"${SH_DIR}/on_ci_publish_tagged_images.sh"
+"${SCRIPTS_DIR}/build_docker_images.sh"
+"${SCRIPTS_DIR}/tag_image.sh"
+"${SCRIPTS_DIR}/docker_containers_up.sh"
+"${SCRIPTS_DIR}/run_tests_in_containers.sh" "$@"
+"${SCRIPTS_DIR}/docker_containers_down.sh"
+"${SCRIPTS_DIR}/on_ci_publish_tagged_images.sh"
