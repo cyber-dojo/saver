18aefac39835f456686ba3ed719e924b1dd94ef9
Jon Jagger <jon@jaggersoft.com>
2021-05-05 08:26:51 +0100

    Refactor: improve readability of id58 alphabet

--- app/test/id58_test_base.rb
+++ app/test/id58_test_base.rb
@@ -73,9 +73,9 @@ class Id58TestBase < MiniTest::Test
 
   ID58_ALPHABET = %w{
     0 1 2 3 4 5 6 7 8 9
-    A B C D E F G H   J K L M N   P Q R S T U V W X Y Z
-    a b c d e f g h   j k l m n   p q r s t u v w x y z
-  }.join.freeze
+    A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+    a b c d e f g h i j k l m n o p q r s t u v w x y z
+  }.join.tr('IiOo','').freeze
 
   def self.id58?(s)
     s.instance_of?(String) &&
