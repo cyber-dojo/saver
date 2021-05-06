32c6823ddcf7b7ae0167588d3590fb84f8404a6b
Jon Jagger <jon@jaggersoft.com>
2021-05-06 07:02:32 +0100

    Tidy coverage helpers

--- client/test/config/coverage.rb
+++ client/test/config/coverage.rb
@@ -1,9 +1,9 @@
 require 'simplecov'
 
-cov_root = File.expand_path('../..', __dir__)
 SimpleCov.start do
-  add_group('code') { |code| code.filename.start_with?("#{cov_root}/source" ) }
-  add_group('test') { |test| test.filename.start_with?("#{cov_root}/test") }
+  cov_root = File.expand_path('../..', __dir__)
+  root(cov_root)
+  coverage_dir(ENV['COVERAGE_ROOT'])
+  add_group('code') { |the| the.filename.start_with?("#{cov_root}/source" ) }
+  add_group('test') { |the| the.filename.start_with?("#{cov_root}/test") }
 end
-SimpleCov.root(cov_root)
-SimpleCov.coverage_dir(ENV['COVERAGE_ROOT'])
