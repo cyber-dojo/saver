255b82a0ff5534f529f51a68015841ae3f6b22a7
Jon Jagger <jon@jaggersoft.com>
2021-05-08 13:41:09 +0100

    Split manifest tests into two files group and kata

---
+++ app/test/group_manifest_test.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+require_relative 'test_base'
+
+class GroupManifestTest < TestBase
+
+  def self.id58_prefix
+    '5Zt'
+  end
+
+  def id58_setup
+    @display_name = custom_start_points.display_names.sample
+    manifest = custom_start_points.manifest(display_name)
+    manifest['version'] = version
+    @custom_manifest = manifest
+  end
+
+  attr_reader :display_name, :custom_manifest
+
+  #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  v_tests [0], '472', %w(
+  already existing group_manifest {test-data copied into saver}
+  ) do
+    manifest = group_manifest(id='chy6BJ')
+    refute manifest.has_key?('version')
+    assert_equal 'Ruby, MiniTest', manifest['display_name']
+    assert_equal 'cyberdojofoundation/ruby_mini_test', manifest['image_name'], :pre_tagging
+    assert_equal ['.rb'], manifest['filename_extension']
+    assert_equal 2, manifest['tab_size']
+    assert_equal [
+      "test_hiker.rb",
+      "hiker.rb",
+      "cyber-dojo.sh",
+      "coverage.rb",
+      "readme.txt"
+    ].sort, manifest['visible_files'].keys.sort
+    assert_equal 'Count_Coins', manifest['exercise']
+    assert_equal [2019,1,19,12,41,0,406370], manifest['created']
+    assert_equal 'chy6BJ', manifest['id']
+  end
+
+  #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  v_tests [0,1], 'Q61', %w(
+  retrieved group_manifest matches saved group_manifest
+  ) do
+    now = [2019,3,17, 7,13,36,3428]
+    externals.instance_exec {
+      @time = TimeStub.new(now)
+    }
+    manifest = custom_manifest
+    id = group_create(manifest, default_options)
+    saved = group_manifest(id)
+    manifest.keys.each do |key|
+      assert_equal manifest[key], saved[key],  key
+    end
+    assert saved.keys.include?('created'), :created_key
+    assert_equal now, saved['created'], :created
+    assert saved.keys.include?('version'), :version_key
+    assert_equal version, saved['version'], :version
+  end
+
+end
--- app/test/kata_manifest_test.rb
+++ app/test/kata_manifest_test.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 require_relative 'test_base'
 
-class ManifestTest < TestBase
+class KataManifestTest < TestBase
 
   def self.id58_prefix
     '5Ks'
@@ -15,29 +15,6 @@ class ManifestTest < TestBase
   end
 
   attr_reader :display_name, :custom_manifest
-
-  #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-  v_tests [0], '472', %w(
-  already existing group_manifest {test-data copied into saver}
-  ) do
-    manifest = group_manifest(id='chy6BJ')
-    refute manifest.has_key?('version')
-    assert_equal 'Ruby, MiniTest', manifest['display_name']
-    assert_equal 'cyberdojofoundation/ruby_mini_test', manifest['image_name'], :pre_tagging
-    assert_equal ['.rb'], manifest['filename_extension']
-    assert_equal 2, manifest['tab_size']
-    assert_equal [
-      "test_hiker.rb",
-      "hiker.rb",
-      "cyber-dojo.sh",
-      "coverage.rb",
-      "readme.txt"
-    ].sort, manifest['visible_files'].keys.sort
-    assert_equal 'Count_Coins', manifest['exercise']
-    assert_equal [2019,1,19,12,41,0,406370], manifest['created']
-    assert_equal 'chy6BJ', manifest['id']
-  end
 
   #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -86,29 +63,6 @@ class ManifestTest < TestBase
 
 
   #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-  #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-  #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-  v_tests [0,1], 'Q61', %w(
-  retrieved group_manifest matches saved group_manifest
-  ) do
-    now = [2019,3,17, 7,13,36,3428]
-    externals.instance_exec {
-      @time = TimeStub.new(now)
-    }
-    manifest = custom_manifest
-    id = group_create(manifest, default_options)
-    saved = group_manifest(id)
-    manifest.keys.each do |key|
-      assert_equal manifest[key], saved[key],  key
-    end
-    assert saved.keys.include?('created'), :created_key
-    assert_equal now, saved['created'], :created
-    assert saved.keys.include?('version'), :version_key
-    assert_equal version, saved['version'], :version
-  end
-
-  #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
   v_tests [0,1], 'Q62', %w(
   retrieved kata_manifest matches saved kata_manifest
@@ -127,15 +81,6 @@ class ManifestTest < TestBase
     assert_equal now, saved['created'], :created
     assert saved.keys.include?('version'), :version_key
     assert_equal version, saved['version'], :version
-  end
-
-  private
-
-  class TimeStub
-    def initialize(now)
-      @now = now
-    end
-    attr_reader :now
   end
 
 end
