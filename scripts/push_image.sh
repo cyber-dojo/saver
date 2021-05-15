05c410192d6a9828fcf55310418dfff9869d51f1
Jon Jagger <jon@jaggersoft.com>
2021-05-15 12:45:11 +0100

    Fix reversed docker tag arguments

--- scripts/push_image.sh
+++ scripts/push_image.sh
@@ -7,7 +7,7 @@ push_image()
   # DOCKER_USER, DOCKER_PASS are in ci context
   echo "${DOCKER_PASS}" | docker login --username "${DOCKER_USER}" --password-stdin
   docker push $(server_image):$(image_tag)
-  docker tag  $(server_image):latest $(server_image):$(image_tag)
+  docker tag  $(server_image):$(image_tag) $(server_image):latest
   docker push $(server_image):latest
   docker logout
 }
