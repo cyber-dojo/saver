eddbaf72b4e5f8575b6c468270c92bcc96887afd
Jon Jagger <jon@jaggersoft.com>
2021-05-15 13:14:14 +0100

    Add missing docker pull to scripts/push_image.sh

--- scripts/push_image.sh
+++ scripts/push_image.sh
@@ -11,6 +11,7 @@ push_image()
     docker push $(server_image):$(image_tag)
   fi
   if [ "${tag}" == latest ]; then
+    docker pull $(server_image):$(image_tag)
     docker tag  $(server_image):$(image_tag) $(server_image):latest
     docker push $(server_image):latest
   fi
