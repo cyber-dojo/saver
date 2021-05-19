6e545ec878016badad11cd8bb8d5e465a23881c0
Jon Jagger <jon@jaggersoft.com>
2021-05-19 15:28:41 +0100

    Add Merkely approve_deployment step to CircleCI pipeline

--- scripts/merkely_approve_deployment.sh
+++ scripts/merkely_approve_deployment.sh
@@ -9,25 +9,32 @@ source "${SCRIPTS_DIR}/merkely_echo_env_vars.sh"
 source "${SCRIPTS_DIR}/merkely_fingerprint.sh"
 
 # - - - - - - - - - - - - - - - - - - -
-merkely_log_deployment()
+merkely_approve_deployment()
 {
-  local -r environment="${1}"
   docker pull $(server_image):$(image_tag)
 	docker run \
-    --env MERKELY_COMMAND=log_deployment \
+    --env MERKELY_COMMAND=approve_deployment \
     --env MERKELY_OWNER=${MERKELY_OWNER} \
     --env MERKELY_PIPELINE=${MERKELY_PIPELINE} \
     --env MERKELY_FINGERPRINT=$(merkely_fingerprint) \
-    --env MERKELY_DESCRIPTION="Deployed to ${environment} in CircleCI pipeline" \
-    --env MERKELY_ENVIRONMENT="${environment}" \
-    --env MERKELY_CI_BUILD_URL=${CIRCLE_BUILD_URL} \
+    --env MERKELY_OLDEST_SRC_COMMITISH=origin/production \
+    --env MERKELY_NEWEST_SRC_COMMITISH=${COMMIT_SHA} \
+    --env MERKELY_DESCRIPTION="Approval created in CircleCI" \
+    --env MERKELY_SRC_REPO_ROOT=${CIRCLE_WORKING_DIRECTORY} \
     --env MERKELY_API_TOKEN=${MERKELY_API_TOKEN} \
     --rm \
     --volume /var/run/docker.sock:/var/run/docker.sock \
     ${MERKELY_CHANGE}
+
+  # For debugging
+  git log --graph --full-history --all --color --date=short --pretty=format:"%Cred%x09%h %Creset%ad%Cblue%d %Creset %s %C(bold)(%an)%Creset" | head -n 30
+  # Update git tracking branch
+  git checkout --track origin/production
+  git merge --ff-only ${COMMIT_SHA}
+  git push origin production
 }
 
 # - - - - - - - - - - - - - - - - - - -
 export $(echo_versioner_env_vars)
 export $(merkely_echo_env_vars)
-merkely_log_deployment "${1}"
+merkely_approve_deployment
