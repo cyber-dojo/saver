df6df51f9ffc6bab4e4001dcea28e1d869471885
Jon Jagger <jon@jaggersoft.com>
2021-06-06 17:53:47 +0100

    Add echoing of env vars

---
+++ scripts/echo_env_vars.sh
@@ -0,0 +1,9 @@
+
+#- - - - - - - - - - - - - - - - - - - - - - - -
+echo_env_vars()
+{
+  echo
+  echo "echo CYBER_DOJO_SAVER_SHA=$(image_sha)"
+  echo "echo CYBER_DOJO_SAVER_TAG=$(image_tag)"
+  echo
+}
