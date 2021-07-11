fd777168275d354e97d89a0c15c4ee795a826654
diego <diego@audisoft.com.br>
2021-07-11 11:06:12 -0300

    Added auto delete temp dir

--- app/source/model/kata_v2.rb
+++ app/source/model/kata_v2.rb
@@ -5,6 +5,7 @@ require_relative 'poly_filler'
 require_relative '../lib/json_adapter'
 require_relative '../lib/tarfile_reader'
 require_relative '../lib/utf8_clean'
+require 'tmpdir'
 
 # 1. Uses git repo to store date
 # 2. event.json has been dropped
@@ -189,17 +190,14 @@ class Kata_v2
     # and the root-dir of the tar command is 'S7'.
     # So using --transform to create root-dir with a name
     # matching the tzg filename itself.
-    # The true tgz filename has a random-id to support concurrent
-    # downloads of the same kata during a mob setting.
-    rid = random.alphanumeric(8)
-    tmp_dir = "/tmp"
     year, month, day = *time.now
     user_name = "cyber-dojo-#{year}-#{month}-#{day}-#{id}"
-    true_name = "#{user_name}-#{rid}"
-    tgz_command = "tar -czf #{tmp_dir}/#{true_name}.tgz --transform s/^./#{user_name}/ ."
-    shell.assert_cd_exec(repo_dir(id), tgz_command)
-    tgz_file_path = "#{tmp_dir}/#{true_name}.tgz"
-    [ tmp_dir, "#{true_name}.tgz", user_name, Base64.encode64(File.read(tgz_file_path)) ]
+    Dir.mktmpdir do | tmp_dir |
+      tgz_command = "tar -czf #{tmp_dir}/#{user_name}.tgz --transform s/^./#{user_name}/ ."
+      shell.assert_cd_exec(repo_dir(id), tgz_command)
+      tgz_file_path = "#{tmp_dir}/#{user_name}.tgz"
+      [ "#{user_name}.tgz", Base64.encode64(File.read(tgz_file_path)) ]
+    end
   end
 
   private
