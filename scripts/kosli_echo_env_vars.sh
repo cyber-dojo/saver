4cdaba634a303d0f04fc3307719aa2e38a6c4a71
Jon Jagger <jon@jaggersoft.com>
2022-08-20 13:46:00 +0100

    CI: update script for CircleCI until we have moved to AWS and Github Actions

--- scripts/kosli_echo_env_vars.sh
+++ scripts/kosli_echo_env_vars.sh
@@ -6,6 +6,14 @@ kosli_echo_env_vars()
   echo MERKELY_OWNER=cyber-dojo
   echo MERKELY_PIPELINE=saver
 
-  echo CI_BUILD_NUM=${GITHUB_RUN_NUMBER}
-  echo CI_BUILD_URL=${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}
+  if [ "${GITHUB_ACTIONS}" == 'true' ]; then
+    echo CI_BUILD_NUM=${GITHUB_RUN_NUMBER}
+    echo CI_BUILD_URL=${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID}
+  fi
+
+  if [ "${CIRCLECI}" == 'true' ]; then
+    echo CI_BUILD_NUM=${CIRCLE_BUILD_NUM}
+    echo CI_BUILD_URL=${CIRCLE_BUILD_URL}
+  fi
+
 }
