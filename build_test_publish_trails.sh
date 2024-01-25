f58b8b210f1ede9d027eb604558501583558fb0a
JonJagger <jon@kosli.com>
2024-01-25 10:44:19 +0000

    Add Kosli trail

---
+++ build_test_publish_trails.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -Eeu
+
+export ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+pushd "${ROOT_DIR}/sh"
+
+source "./on_ci_publish_images.sh"
+source "./kosli_trails.sh"
+source "./echo_versioner_env_vars.sh"
+export $(echo_versioner_env_vars)
+
+on_ci_kosli_create_trail
+
+./build.sh
+on_ci_publish_images
+on_ci_kosli_attest_artifact
+
+./up.sh
+./wait.sh
+./test.sh "$@"
+on_ci_kosli_attest_coverage_evidence
+on_ci_kosli_attest_snyk_scan_evidence
+on_ci_kosli_assert_artifact
+on_ci_publish_images latest
+
+popd
---
+++ sh/kosli_trails.sh
@@ -0,0 +1,199 @@
+#!/usr/bin/env bash
+set -Eeu
+
+export KOSLI_FLOW=saver
+export KOSLI_ORG=cyber-dojo-trails
+# KOSLI_API_TOKEN_TRAILS is set in CI
+# KOSLI_API_TOKEN_TRAILS_STAGING is set in CI
+# KOSLI_HOST_STAGING is set in CI
+# KOSLI_HOST_PRODUCTION is set in CI
+# SNYK_TOKEN is set in CI
+
+# - - - - - - - - - - - - - - - - - - -
+kosli_create_trail()
+{
+  local -r hostname="${1}"
+  local -r api_token="${2}"
+
+  kosli create flow "${KOSLI_FLOW}" \
+    --description="Group/Kata model+persistence" \
+    --host="${hostname}" \
+    --api-token="${api_token}" \
+    --template-file=.kosli.yml \
+    --visibility=public
+
+  kosli begin trail "${GITHUB_SHA}"
+}
+
+# - - - - - - - - - - - - - - - - - - -
+kosli_attest_artifact()
+{
+  local -r hostname="${1}"
+  local -r api_token="${2}"
+
+  pushd "$(root_dir)"  # So we don't need --repo-root flag
+
+  kosli attest artifact "$(artifact_name)" \
+    --artifact-type=docker \
+    --host="${hostname}" \
+    --api-token="${api_token}" \
+    --name=saver \
+    --trail="${GITHUB_SHA}"
+
+  popd
+}
+
+# - - - - - - - - - - - - - - - - - - -
+kosli_attest_coverage_evidence()
+{
+  local -r hostname="${1}"
+  local -r api_token="${2}"
+
+  kosli attest generic "$(artifact_name)" \
+    --artifact-type=docker \
+    --description="server & client branch-coverage reports" \
+    --name=saver.branch-coverage \
+    --user-data="$(coverage_json_path)" \
+    --host="${hostname}" \
+    --api-token="${api_token}" \
+    --trail="${GITHUB_SHA}"
+
+}
+
+# - - - - - - - - - - - - - - - - - - -
+kosli_attest_snyk_evidence()
+{
+  local -r hostname="${1}"
+  local -r api_token="${2}"
+
+  kosli attest snyk "$(artifact_name)" \
+    --artifact-type=docker \
+    --host="${hostname}" \
+    --api-token="${api_token}" \
+    --name=saver.snyk-scan \
+    --scan-results="$(root_dir)/snyk.json" \
+    --trail="${GITHUB_SHA}"
+}
+
+# - - - - - - - - - - - - - - - - - - -
+kosli_assert_artifact()
+{
+  local -r hostname="${1}"
+  local -r api_token="${2}"
+
+  kosli assert artifact "$(artifact_name)" \
+    --artifact-type=docker \
+    --host="${hostname}" \
+    --api-token="${api_token}"
+}
+
+# - - - - - - - - - - - - - - - - - - -
+kosli_expect_deployment()
+{
+  local -r environment="${1}"
+  local -r hostname="${2}"
+  local -r api_token="${3}"
+
+  # In .github/workflows/main.yml deployment is its own job
+  # and the image must be present to get its sha256 fingerprint.
+  docker pull "$(artifact_name)"
+
+  kosli expect deployment "$(artifact_name)" \
+    --artifact-type=docker \
+    --description="Deployed to ${environment} in Github Actions pipeline" \
+    --environment="${environment}" \
+    --host="${hostname}" \
+    --api-token="${api_token}"
+}
+
+# - - - - - - - - - - - - - - - - - - -
+on_ci_kosli_create_trail()
+{
+  if on_ci; then
+    kosli_create_trail "${KOSLI_HOST_STAGING}"    "${KOSLI_API_TOKEN_TRAILS_STAGING}"
+    kosli_create_trail "${KOSLI_HOST_PRODUCTION}" "${KOSLI_API_TOKEN_TRAILS}"
+  fi
+}
+
+# - - - - - - - - - - - - - - - - - - -
+on_ci_kosli_report_artifact()
+{
+  if on_ci; then
+    kosli_attest_artifact "${KOSLI_HOST_STAGING}"    "${KOSLI_API_TOKEN_TRAILS_STAGING}"
+    kosli_attest_artifact "${KOSLI_HOST_PRODUCTION}" "${KOSLI_API_TOKEN_TRAILS}"
+  fi
+}
+
+# - - - - - - - - - - - - - - - - - - -
+on_ci_kosli_report_coverage_evidence()
+{
+  if on_ci; then
+    write_coverage_json
+    kosli_attest_coverage_evidence "${KOSLI_HOST_STAGING}"    "${KOSLI_API_TOKEN_TRAILS_STAGING}"
+    kosli_attest_coverage_evidence "${KOSLI_HOST_PRODUCTION}" "${KOSLI_API_TOKEN_TRAILS}"
+  fi
+}
+
+# - - - - - - - - - - - - - - - - - - -
+on_ci_kosli_report_snyk_scan_evidence()
+{
+  if on_ci; then
+    set +e
+    snyk container test "$(artifact_name)" \
+      --json-file-output="$(root_dir)/snyk.json" \
+      --policy-path="$(root_dir)/.snyk"
+    set -e
+
+    kosli_attest_snyk_evidence "${KOSLI_HOST_STAGING}"    "${KOSLI_API_TOKEN_TRAILS_STAGING}"
+    kosli_attest_snyk_evidence "${KOSLI_HOST_PRODUCTION}" "${KOSLI_API_TOKEN_TRAILS}"
+  fi
+}
+
+# - - - - - - - - - - - - - - - - - - -
+on_ci_kosli_assert_artifact()
+{
+  if on_ci; then
+    kosli_assert_artifact "${KOSLI_HOST_STAGING}"    "${KOSLI_API_TOKEN_TRAILS_STAGING}"
+    kosli_assert_artifact "${KOSLI_HOST_PRODUCTION}" "${KOSLI_API_TOKEN_TRAILS}"
+  fi
+}
+
+
+# - - - - - - - - - - - - - - - - - - -
+artifact_name()
+{
+  source "$(root_dir)/sh/echo_versioner_env_vars.sh"
+  export $(echo_versioner_env_vars)
+  echo "${CYBER_DOJO_SAVER_IMAGE}:${CYBER_DOJO_SAVER_TAG}"
+}
+
+# - - - - - - - - - - - - - - - - - - -
+root_dir()
+{
+  git rev-parse --show-toplevel
+}
+export -f root_dir
+
+# - - - - - - - - - - - - - - - - - - -
+on_ci()
+{
+  [ -n "${CI:-}" ]
+}
+
+# - - - - - - - - - - - - - - - - - - -
+write_coverage_json()
+{
+  {
+    echo '{ "server":'
+    cat "$(root_dir)/tmp/coverage/server/coverage.json"
+    echo ', "client":'
+    cat "$(root_dir)/tmp/coverage/client/coverage.json"
+    echo '}'
+  } > "$(coverage_json_path)"
+}
+
+# - - - - - - - - - - - - - - - - - - -
+coverage_json_path()
+{
+  echo "$(root_dir)/tmp/evidence.json"
+}
