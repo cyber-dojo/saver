635232cc67ccfa05a10a212decde09c337e7ee82
JonJagger <jon@kosli.com>
2024-11-01 20:30:40 +0000

    Update todo file

--- bin/wait.sh
+++ bin/wait.sh
@@ -2,41 +2,8 @@
 set -Eeu
 
 pushd "${ROOT_DIR}/bin"
-source "./config.sh"
 source "./echo_versioner_env_vars.sh"
 popd
-
-# - - - - - - - - - - - - - - - - - - -
-#exit_non_zero_unless_healthy()
-#{
-#  local -r CONTAINER_NAME="${1}"
-#  local -r SERVICE_NAME="${2}"
-#  local -r MAX_TRIES=50
-#
-#  echo
-#  printf "Waiting until ${SERVICE_NAME} is healthy"
-#  for _ in $(seq ${MAX_TRIES})
-#  do
-#    if healthy "${CONTAINER_NAME}"; then
-#      echo; echo "${SERVICE_NAME} is healthy."
-#      return
-#    else
-#      printf .
-#      sleep 0.1
-#    fi
-#  done
-#  echo; echo "${SERVICE_NAME} not healthy after ${MAX_TRIES} tries."
-#  local log=$(docker logs "${CONTAINER_NAME}" 2>&1)
-#  echo_docker_log "${SERVICE_NAME}" "${log}"
-#  echo
-#  exit 42
-#}
-
-# - - - - - - - - - - - - - - - - - - -
-#healthy()
-#{
-#  docker ps --filter health=healthy --format '{{.Names}}' | grep -q "${CONTAINER_NAME}"
-#}
 
 # - - - - - - - - - - - - - - - - - - -
 strip_known_warning()
@@ -57,11 +24,11 @@ exit_non_zero_unless_clean()
   local -r name="${1}"
   local log=$(docker logs "${name}" 2>&1)
 
-  if [ "${name}" == saver_custom-start-points_1 ]; then
-    local -r lines=6
-  else
-    local -r lines=8
-  fi
+  #if [ "${name}" == saver_custom-start-points_1 ]; then
+  #  local -r lines=6
+  #else
+  local -r lines=8
+  #fi
 
   #local -r mismatched_indent_warning="application(.*): warning: mismatched indentations at 'rescue' with 'begin'"
   #log=$(strip_known_warning "${log}" "${mismatched_indent_warning}")
@@ -93,14 +60,10 @@ echo_docker_log()
 # - - - - - - - - - - - - - - - - - - - -
 containers_wait()
 {
-  exit_non_zero_unless_healthy $(server_container) $(server_name)
-  exit_non_zero_unless_clean   $(server_container)
-
-  exit_non_zero_unless_healthy $(client_container) $(client_name)
-  exit_non_zero_unless_clean   $(client_container)
+  exit_non_zero_unless_clean ${CYBER_DOJO_SAVER_SERVER_CONTAINER_NAME}
+  exit_non_zero_unless_clean ${CYBER_DOJO_SAVER_CLIENT_CONTAINER_NAME}
 }
 
 # - - - - - - - - - - - - - - - - - - - -
 export $(echo_versioner_env_vars)
-#exit_non_zero_unless_installed docker
 containers_wait
