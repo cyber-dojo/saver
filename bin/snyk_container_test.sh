2b3a1fd3d7efa5e4c7ba77f75c8d79fa740315b8
JonJagger <jon@kosli.com>
2024-11-01 15:41:07 +0000

    Update workflow

---
+++ bin/snyk_container_test.sh
@@ -0,0 +1,16 @@
+
+stderr()
+{
+  local -r message="${1}"
+  >&2 echo "ERROR: ${message}"
+}
+
+exit_non_zero_unless_file_exists()
+{
+  local -r filename="${1}"
+  if [ ! -f "${filename}" ]; then
+    stderr "${filename} does not exist"
+    exit 42
+  fi
+}
+export -f exit_non_zero_unless_file_exists
