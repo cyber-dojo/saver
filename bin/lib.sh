83a6af0a10fc7ca6f87722f04bb09b90dce76ac8
JonJagger <jon@kosli.com>
2025-02-24 13:18:32 +0000

    Remove workflow job now handler by versioner

--- bin/lib.sh
+++ bin/lib.sh
@@ -100,7 +100,7 @@ exit_non_zero_unless_installed()
   for dependent in "$@"
   do
     if ! installed "${dependent}" ; then
-      stderr "${dependent}" is not installed
+      stderr "${dependent} is not installed"
       exit 42
     fi
   done
