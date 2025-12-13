f30eaea855dfe2102f6f87b7482dc98be6a75752
JonJagger <jon@kosli.com>
2025-12-13 08:57:26 +0000

    Update test metrics limits

--- test/server/config/test_metrics_limits.rb
+++ test/server/config/test_metrics_limits.rb
@@ -2,7 +2,7 @@
 def metrics
   [
     [ nil ],
-    [ 'test_count',    '>=', 316 ],
+    [ 'test_count',    '>=', 319 ],
     [ 'total_time',    '<=', 100 ],
     [ nil ],
     [ 'failure_count', '<=', 0   ],
