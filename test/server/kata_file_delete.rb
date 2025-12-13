3d10aa0dd44b7ca0473895649da505bc126d51ab
JonJagger <jon@kosli.com>
2025-12-13 10:17:41 +0000

    Add new test for file-delete

--- test/server/kata_file_delete.rb
+++ test/server/kata_file_delete.rb
@@ -14,7 +14,7 @@ class KataFileDeleteTest < TestBase
   # - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
   test 'C01', %w(
-  |when no other file has been edited
+  |when no files have been edited
   |a kata_file_delete event
   |results in a single delete-file event 
   ) do
@@ -38,11 +38,12 @@ class KataFileDeleteTest < TestBase
   # - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
   test 'C02', %w(
-  |when one other file has been edited
+  |when one file has been edited
+  |and a different file has been deleted
   |a kata_file_delete event 
   |results in two events
   |the first for the edit
-  |the second for the deleted file
+  |the second for the delete
   ) do
     in_tennis_kata do |id, files|
       edited_content = files['readme.txt']['content'] + 'Hello world'
@@ -73,6 +74,38 @@ class KataFileDeleteTest < TestBase
 
   # - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-  # TODO: what about when the deleted file has also been edited?
-  
+  test 'C03', %w(
+  |when one file has been edited
+  |and the same file has been deleted
+  |a kata_file_delete event 
+  |results in two events
+  |the first for the edit
+  |the second for the delete
+  ) do
+    in_tennis_kata do |id, files|
+      edited_content = files['readme.txt']['content'] + 'Hello world'
+      files['readme.txt']['content'] = edited_content
+      kata_file_delete(id, index=1, files, 'readme.txt')
+
+      events = kata_events(id)
+      assert_equal 3, events.size
+
+      event1 = events[1]
+      assert_equal 1, event1['index']
+      assert_equal 'edit-file', event1['colour']
+      assert_equal 'readme.txt', event1['filename']
+      files = kata_event(id, 1)['files']
+      assert_equal edited_content, files['readme.txt']['content']
+      assert_tag_commit_message(id, 1, '1 edited file readme.txt')
+
+      event2 = events[2]
+      assert_equal 2, event2['index']
+      assert_equal 'delete-file', event2['colour']
+      assert_equal 'readme.txt', event2['filename']
+      files = kata_event(id, 2)['files']
+      filenames = files.keys
+      refute filenames.include?('readme.txt')
+      assert_tag_commit_message(id, 2, '2 deleted file readme.txt')
+    end
+  end  
 end
