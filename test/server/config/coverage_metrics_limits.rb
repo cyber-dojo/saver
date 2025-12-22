adaf9a8dd9a71175aef1a62e774dc84526f9cf08
JonJagger <jon@kosli.com>
2025-12-22 14:52:36 +0000

    Add missing test

--- test/server/config/coverage_metrics_limits.rb
+++ test/server/config/coverage_metrics_limits.rb
@@ -2,14 +2,14 @@
 def metrics
   [
     [ nil ],
-    [ 'test.lines.total'    , '<=', 1971 ],
+    [ 'test.lines.total'    , '<=', 1977 ],
     [ 'test.lines.missed'   , '<=', 0    ],
     [ 'test.branches.total' , '<=', 12   ],
     [ 'test.branches.missed', '<=', 0    ],
     [ nil ],
     [ 'code.lines.total'    , '<=', 1316 ],
-    [ 'code.lines.missed'   , '<=', 12   ],
+    [ 'code.lines.missed'   , '<=', 11   ],
     [ 'code.branches.total' , '<=', 155  ],
-    [ 'code.branches.missed', '<=', 4    ],
+    [ 'code.branches.missed', '<=', 3    ],
   ]
 end
--- test/server/config/test_metrics_limits.rb
+++ test/server/config/test_metrics_limits.rb
@@ -2,7 +2,7 @@
 def metrics
   [
     [ nil ],
-    [ 'test_count',    '>=', 319 ],
+    [ 'test_count',    '>=', 320 ],
     [ 'total_time',    '<=', 100 ],
     [ nil ],
     [ 'failure_count', '<=', 0   ],
