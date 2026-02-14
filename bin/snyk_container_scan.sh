035f6c7574691ae91d0317b1a09d64fb871c401c
Jon Jagger <jon@kosli.com>
2026-02-14 11:25:47 +0000

    Add snyk workflow (#303)
    * Add snyk workflow
    
    * Fix workflow yaml
    
    * Add info on whether sarif file is created or not
    
    * Print sarif json file if it is created
    
    * Redirect stdout and stderr of snyk scan to file
    
    * Rerun workflow after updating snyk token
    
    * Silence rm command
    
    * Restore main workflow
    
    * Update error checking

--- bin/snyk_container_scan.sh
+++ bin/snyk_container_scan.sh
@@ -7,12 +7,70 @@ source "${ROOT_DIR}/bin/echo_env_vars.sh"
 # shellcheck disable=SC2046
 export $(echo_env_vars)
 
-readonly IMAGE_NAME="${CYBER_DOJO_SAVER_IMAGE}:${CYBER_DOJO_SAVER_TAG}"
 readonly SARIF_FILENAME=${SARIF_FILENAME:-snyk.container.scan.json}
+readonly SNYK_LOG_FILENAME="${ROOT_DIR}/snyk.log"
+
+function image_name()
+{
+  if [ "${IMAGE_NAME:-}" == '' ]; then
+    echo "${CYBER_DOJO_SAVER_IMAGE}:${CYBER_DOJO_SAVER_TAG}"
+  else 
+    echo "${IMAGE_NAME}"
+  fi
+}
+
+function sarif_file_exists()
+{
+  if [ -f "${ROOT_DIR}/${SARIF_FILENAME}" ]; then
+    echo true
+  else 
+    echo false
+  fi
+}
 
 exit_non_zero_unless_installed snyk
 
-snyk container test "${IMAGE_NAME}" -debug \
+rm "${ROOT_DIR}/${SARIF_FILENAME}" &> /dev/null || true
+
+set +e
+snyk container test "$(image_name)" -debug \
   --policy-path="${ROOT_DIR}/.snyk" \
   --sarif \
-  --sarif-file-output="${ROOT_DIR}/${SARIF_FILENAME}"
+  --sarif-file-output="${ROOT_DIR}/${SARIF_FILENAME}" \
+  &> "${SNYK_LOG_FILENAME}"
+STATUS=$?
+set -e
+
+if grep Forbidden "${SNYK_LOG_FILENAME}" ; then
+  cat "${SNYK_LOG_FILENAME}"
+  echo
+  echo '============================================='
+  echo ERROR: Snyk log contains 'Forbidden'
+  echo "Sarif file exists?: $(sarif_file_exists)"
+  echo "Snyk exit status: ${STATUS}"
+  echo '============================================='
+  echo
+  exit 42
+fi
+
+if grep 'Authentication error' "${SNYK_LOG_FILENAME}" ; then
+  cat "${SNYK_LOG_FILENAME}"
+  echo
+  echo '============================================='
+  echo ERROR: Snyk log contains 'Authentication error'
+  echo "Sarif file exists?: $(sarif_file_exists)"
+  echo "Snyk exit status: ${STATUS}"
+  echo '============================================='
+  echo
+  exit 43
+fi
+
+echo "Snyk exit status: ${STATUS}"
+echo "Sarif file exists?: $(sarif_file_exists)"
+
+if [ "$(sarif_file_exists)" == 'true' ]; then 
+  echo
+  jq . "${ROOT_DIR}/${SARIF_FILENAME}"
+fi
+
+exit "${STATUS}"
