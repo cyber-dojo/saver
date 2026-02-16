abdf163c99cf4fb3e4fe46e17bbe0a70ac52354e
Jon Jagger <jon@kosli.com>
2026-02-16 12:14:25 +0000

    Fix snyk version (#309)
    * Redirect stdout stderr to separate files in snyk-scan
    
    * Inline actions in snyk-scan workflow job
    
    * Add missing permissions to snyk workflow job
    
    * Do checkout earlier
    
    * Run with no snyk-test -debug flag
    
    * Run again with the -debug flag
    
    * Fix snyk version in snyk workflow to 1.1300.2
    
    * Fix snyk version in main workflow

--- bin/snyk_container_scan.sh
+++ bin/snyk_container_scan.sh
@@ -10,7 +10,7 @@ export $(echo_env_vars)
 readonly SARIF_FILENAME=${SARIF_FILENAME:-snyk.container.scan.json}
 readonly SNYK_LOG_FILENAME="${ROOT_DIR}/snyk.log"
 
-function image_name()
+function echo_image_name()
 {
   if [ "${IMAGE_NAME:-}" == '' ]; then
     echo "${CYBER_DOJO_SAVER_IMAGE}:${CYBER_DOJO_SAVER_TAG}"
@@ -22,9 +22,23 @@ function image_name()
 function sarif_file_exists()
 {
   if [ -f "${ROOT_DIR}/${SARIF_FILENAME}" ]; then
-    echo true
+    return 0 # true
   else 
-    echo false
+    return 1 # false
+  fi
+}
+
+function fail_if_log_contains()
+{
+  local -r filename="${1}"
+  local -r find="${2}"
+  if grep "${find}" "${filename}" ; then
+    cat "${filename}"
+    echo
+    echo ERROR: ${filename} contains ${find}
+    echo "image_name=$(echo_image_name)"
+    echo
+    EXIT_STATUS=42
   fi
 }
 
@@ -34,11 +48,13 @@ rm "${ROOT_DIR}/${SARIF_FILENAME}" &> /dev/null || true
 
 set +e
 
-snyk container test "$(image_name)" -debug \
+snyk container test "$(echo_image_name)" -debug \
+  --org=jonjagger \
   --policy-path="${ROOT_DIR}/.snyk" \
   --sarif \
   --sarif-file-output="${ROOT_DIR}/${SARIF_FILENAME}" \
-  &> "${SNYK_LOG_FILENAME}"
+  > "${SNYK_LOG_FILENAME}.stdout" \
+ 2> "${SNYK_LOG_FILENAME}.stderr" 
 
 SNYK_STATUS=$?
 
@@ -46,32 +62,20 @@ set -e
 
 EXIT_STATUS="${SNYK_STATUS}"
 
-if grep Forbidden "${SNYK_LOG_FILENAME}" ; then
-  cat "${SNYK_LOG_FILENAME}"
-  echo
-  echo '============================================='
-  echo ERROR: Snyk log contains 'Forbidden'
-  echo '============================================='
-  echo
-  EXIT_STATUS=42
-fi
+fail_if_log_contains "${SNYK_LOG_FILENAME}.stdout" Forbidden
+fail_if_log_contains "${SNYK_LOG_FILENAME}.stderr" Forbidden
 
-if grep 'Authentication error' "${SNYK_LOG_FILENAME}" ; then
-  cat "${SNYK_LOG_FILENAME}"
-  echo
-  echo '============================================='
-  echo ERROR: Snyk log contains 'Authentication error'
-  echo '============================================='
-  echo
-  EXIT_STATUS=43
-fi
+fail_if_log_contains "${SNYK_LOG_FILENAME}.stdout" 'Authentication error'
+fail_if_log_contains "${SNYK_LOG_FILENAME}.stderr" 'Authentication error'
 
 echo "Snyk exit status: ${SNYK_STATUS}"
 
-echo "Sarif file exists?: $(sarif_file_exists)"
-if [ "$(sarif_file_exists)" == 'true' ]; then 
-  echo
+echo -n "Sarif file exists?: "
+if sarif_file_exists ; then 
+  echo true
   jq . "${ROOT_DIR}/${SARIF_FILENAME}"
+else
+  echo false
 fi
 
 exit "${EXIT_STATUS}"
