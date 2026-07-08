4b295b9489b317d940a24bcd5fbe034ed5f37588
Jon Jagger <jon@kosli.com>
2026-07-08 20:20:15 +0100

    Update kosli template with provenance facts+decision (#422)

--- bin/lib.sh
+++ bin/lib.sh
@@ -127,10 +127,9 @@ remove_all_but_latest()
   for image_name in $(echo "${docker_image_ls}" | grep "${name}:")
   do
     if [ "${image_name}" != "${name}:latest" ]; then
-      docker image rm --force "${image_name}"
+      docker image rm --force "${image_name}" || echo "  skipped ${image_name} (in use)"
     fi
   done
-  docker system prune --force
 }
 
 echo_warnings()
